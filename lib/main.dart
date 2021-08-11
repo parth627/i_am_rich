bfef8e8ada55c729c02afc2ff3cf6d843e391190
Parth <parthlangalia627@gmail.com>
2021-08-11 20:22:11 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+// main function begins.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor:Colors.blueGrey,
+        appBar:AppBar(
+          title: Center(
+            child:Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900]
+        ),
+        body: Center(
+          child: Image(
+            image:AssetImage('images/ship.bmp'),
+          ),
+        )
+      ),
+    ),
+  );
+}
